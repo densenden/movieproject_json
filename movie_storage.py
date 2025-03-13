8da94775bb6558448ed3d407e3cc17a9849507e2
densenden <git@deniskreuzer.dk>
2025-03-13 19:40:03 +0100

    new docstrings

--- movie_storage.py
+++ movie_storage.py
@@ -1,22 +1,41 @@
 import json
 
 def storage_load_movies():
+    """
+    Load the movie database from a file.
+    """
     try:
         with open("data.json", "r") as f:
             return json.load(f)
     except (FileNotFoundError, json.JSONDecodeError):
         return {}
 
+
 def storage_save_movies(movie_database):
+    """
+    Save the movie database to a file.
+    """
     with open("data.json", "w") as f:
         json.dump(movie_database, f, indent=4)
 
+
 def storage_add_movie(title, rating, year):
+    """
+    Add a movie to the database.
+    :param title:
+    :param rating:
+    :param year:
+    :return:
+    """
     movie_database = storage_load_movies()
     movie_database[title] = {"rating": rating, "year": year}
     storage_save_movies(movie_database)
 
+
 def storage_delete_movie(title):
+    """
+    Delete a movie from the database.
+    """
     movie_database = storage_load_movies()
     if title in movie_database:
         del movie_database[title]
@@ -24,7 +43,11 @@ def storage_delete_movie(title):
         return True
     return False
 
+
 def storage_update_movie(title, new_rating):
+    """"
+    Update the rating of a movie in the database.
+    """
     movie_database = storage_load_movies()
     if title in movie_database:
         movie_database[title]["rating"] = new_rating
@@ -32,10 +55,18 @@ def storage_update_movie(title, new_rating):
         return True
     return False
 
+
 def storage_list_movies():
+    """
+    List all movies plainly from the database.
+    """
     return storage_load_movies()
 
+
 def storage_filter_movies(min_rating, start_year, end_year):
+    """
+    Filter movies by rating and year range.
+    """
     movie_database = storage_load_movies()
     return {
         title: info
